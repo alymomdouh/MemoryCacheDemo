80e4f952bee34d021d5ee30f704fe911683e94ef
Aly Mamdouh <aly.momdouh@gmail.com>
2023-05-27 15:34:15 +0300

    inject AddMemoryCache in program

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ namespace API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddMemoryCache();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
